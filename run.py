1d5e9361ffb10645913996d63613651a673ba2d3
Your Name <you@example.com>
2021-03-26 08:04:06 +0000

    add option to evaluate agent

--- run.py
+++ run.py
@@ -6,11 +6,12 @@ from stable_baselines import PPO2, SAC
 from stable_baselines.common.callbacks import EvalCallback
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines.common.evaluation import evaluate_policy
 
 from gym_foo import gym_foo
 
 
-def train(model_name, total_timesteps=100000, load_path=None):
+def train(model_name, total_timesteps=100000, load_path=None, eval=False):
     env = DummyVecEnv([lambda: gym.make('QuadTakeOffHoverEnv-v0')])
     eval_callback = EvalCallback(
         env, best_model_save_path='./logs', eval_freq=1000, verbose=1)
@@ -23,8 +24,11 @@ def train(model_name, total_timesteps=100000, load_path=None):
         model = model_class('MlpPolicy', env, verbose=1,
                             tensorboard_log='./tensorboard')
 
-    model.learn(total_timesteps=total_timesteps, callback=eval_callback)
-    model.save('quad_model_'+model_name)
+    if eval:
+        evaluate_policy(model, env)
+    else:
+        model.learn(total_timesteps=total_timesteps, callback=eval_callback)
+        model.save('quad_model_'+model_name)
 
 
 if __name__ == "__main__":
@@ -33,6 +37,8 @@ if __name__ == "__main__":
         "--load_path", help="Path to load trained model", type=str, default=None)
     parser.add_argument(
         "--model_name", help="RL model to use", type=str, default=None)
+    parser.add_argument(
+        "--eval", help="Whether to evaluate policy", action='store_true', default=False)
     args = parser.parse_args()
     args = vars(args)
 
